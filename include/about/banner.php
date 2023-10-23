ef8f887ec336792eaa730c37576b6738c8ad1035
Rittik Chawla <rittik.ch@hotmail.com>
2023-10-24 00:41:35 +0700

    project-03

---
+++ include/about/banner.php
@@ -0,0 +1,8 @@
+<div class="about-banner">
+    <div>
+        <h1 class="about-banner-header">
+            About Us </h1>
+
+    </div>
+
+</div>
---
+++ include/home/banner.php
@@ -0,0 +1,49 @@
+<div class="main-image">
+    <div>
+        <h1 class="banner_content">
+            MAKE SURE YOUR HOME IS SAFE </h1>
+        <p class="banner_description">Schedule a new Home or existing home inspection service in MD, Northern VA,
+            and DC today!</p>
+        <a href="#" class="button_banner"> SCEHDULE NOW </a>
+
+    </div>
+
+</div>
+
+<div class="bottom_banner">
+
+    <div class="sliderinspection">
+        <div class="container-fluid">
+            <div class="row row_inspection">
+                <div class="col-md-3 bottom_inspection">
+                    <img src="assets/img/home/homeinspection.svg" alt=""><br>
+                    <p>
+                        HOME<br>INSPECTION
+                    </p>
+                </div>
+                <div class="col-md-3 bottom_inspection">
+                    <img src="assets/img/home/radoninspection.svg" alt=""><br>
+                    <p>
+                        RADON<br>INSPECTION
+                    </p>
+                </div>
+                <div class="col-md-3 bottom_inspection">
+                    <img src="assets/img/home/leadinspection.svg" alt=""><br>
+                    <p>
+                        LEAD<br>INSPECTION
+                    </p>
+                </div>
+                <div class="col-md-3 bottom_inspection">
+                    <img src="assets/img/home/poolinspection.svg" alt=""><br>
+                    <p>
+                        POOL<br>INSPECTION
+                    </p>
+                </div>
+
+            </div>
+
+
+        </div>
+
+    </div>
+</div>
